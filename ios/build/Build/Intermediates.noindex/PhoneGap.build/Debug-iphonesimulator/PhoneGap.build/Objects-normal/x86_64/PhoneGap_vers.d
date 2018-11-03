f420e08664d2cf902a0966a341ff20dd4c321852
Pierrick Coulon <coulonpierrick@gmail.com>
2018-11-03 18:46:24 +0100

    change name root

--- ios/build/Build/Intermediates.noindex/PhoneGap.build/Debug-iphonesimulator/PhoneGap.build/Objects-normal/x86_64/PhoneGap_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/pierrick/Desktop/JU/Development\ Mobile/PhoneGap/ios/build/Build/Intermediates.noindex/PhoneGap.build/Debug-iphonesimulator/PhoneGap.build/DerivedSources/PhoneGap_vers.c
--- ios/build/Build/Intermediates.noindex/PhoneGap.build/Debug-iphonesimulator/PhoneGap.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pierrick/Desktop/JU/Development\ Mobile/PhoneGap/ios/PhoneGap/main.m \
-  /Users/pierrick/Desktop/JU/Development\ Mobile/PhoneGap/ios/PhoneGap/AppDelegate.h
